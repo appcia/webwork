43b4dd129ee355b7f8d20edf6ab9d061a65aafc5
ciapunek <ciapunek@gmail.com>
2013-02-26 03:03:59 +0100

    Strip tags filter

---
+++ lib/Appcia/Webwork/Data/Filter/StripTags.php
@@ -0,0 +1,47 @@
+<?
+
+namespace Appcia\Webwork\Data\Filter;
+
+use Appcia\Webwork\Data\Filter;
+
+class StripTags extends Filter {
+
+    /**
+     * @var string
+     */
+    private $allowedTags;
+
+    /**
+     * Constructor
+     */
+    public function __construct(array $allowed = array())
+    {
+        if (!empty($allowed)) {
+            $this->allowedTags = $this->mergeTags($allowed);
+        }
+    }
+
+    /**
+     * Prepare correct tags format
+     *
+     * @param array $tags Allowed tags
+     *
+     * @return string
+     */
+    private function mergeTags(array $tags) {
+        $value = '';
+        foreach ($tags as $tag) {
+            $value .= '<' . $tag . '>';
+        }
+
+        return $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filter($data) {
+        return strip_tags($data, $this->allowedTags);
+    }
+
+}
