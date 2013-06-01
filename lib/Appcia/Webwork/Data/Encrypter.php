b3db1fdb93d73978aa489403dbbc16be7d771eaa
appcia <appcia.dev@gmail.com>
2013-06-01 19:39:36 +0200

    Encrypter

---
+++ lib/Appcia/Webwork/Data/Encrypter.php
@@ -0,0 +1,152 @@
+<?
+
+namespace Appcia\Webwork\Data;
+
+/**
+ * Data encrypter
+ *
+ * @package Appcia\Webwork\Data
+ */
+class Encrypter
+{
+    /**
+     * Methods
+     */
+    const SHA1 = 'sha1';
+    const SHA256 = 'sha256';
+    const MD5 = 'md5';
+    const CRC32 = 'crc32';
+    const TIGER128 = 'tiger128,3';
+
+    /**
+     * @var array
+     */
+    private static $methods = array(
+        self::SHA1,
+        self::SHA256,
+        self::MD5,
+        self::CRC32,
+        self::TIGER128
+    );
+
+    /**
+     * Encryption method
+     *
+     * @var string
+     */
+    private $method;
+
+    /**
+     * Salt
+     *
+     * @var string
+     */
+    private $salt;
+
+    /**
+     * Constructor
+     *
+     * @param string      $method
+     * @param string|null $salt
+     */
+    public function __construct($method, $salt = null)
+    {
+        if ($salt === null) {
+            $salt = $this->generateSalt();
+        }
+
+        $this->setMethod($method);
+        $this->setSalt($salt);
+    }
+
+    /**
+     * Generate random salt
+     *
+     * @return string
+     */
+    public static function generateSalt()
+    {
+        mt_srand(microtime(true) * 100000 + memory_get_usage(true));
+        $salt = md5(uniqid(mt_rand(), true));
+
+        return $salt;
+    }
+
+    /**
+     * Get available methods
+     *
+     * @return array
+     */
+    public static function getMethods()
+    {
+        return self::$methods;
+    }
+
+    /**
+     * Get method
+     *
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    /**
+     * Set method
+     *
+     * @param string $method
+     *
+     * @return Encrypter
+     * @throws \InvalidArgumentException
+     */
+    public function setMethod($method)
+    {
+        if (!in_array($method, self::$methods)) {
+            throw new \InvalidArgumentException(sprintf("Encryption method '%s' is invalid or unsupported.", $method));
+        }
+
+        $this->method = $method;
+
+        return $this;
+    }
+
+    /**
+     * Crypt data
+     *
+     * @param string $value Value
+     *
+     * @return string
+     */
+    public function crypt($value)
+    {
+        $data = $this->salt . $value;
+        $value = hash($this->method, $data);
+
+        return $value;
+    }
+
+    /**
+     * Get salt
+     *
+     * @return string
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * Set salt
+     *
+     * @param string $salt Salt
+     *
+     * @return Encrypter
+     */
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+
+        return $this;
+    }
+}
